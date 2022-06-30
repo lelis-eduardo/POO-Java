0c7c5cb499c605e4cb675a271e47b80122b2512a
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2022-06-30 08:44:51 -0300

    Fechamento parte 3 do projeto Clinica Medica.

---
+++ ClinicaExames_Parte4/src/Paciente.java
@@ -0,0 +1,65 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Paciente {
+	
+	// Atributos
+	private String nome;
+	private String email;
+	private LocalDate dataNascimento;
+	private String id;
+	private static int numId;
+	private DateTimeFormatter formatter;
+	
+	//Métodos
+	//Construtor
+	public Paciente(String nome, String email, String dataNascimento){
+		
+		this.nome = nome;
+		this.email = email;
+		this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.dataNascimento = LocalDate.parse(dataNascimento, this.formatter);
+		this.id = montaIdentificador();
+	} // end construtor
+
+	public Paciente(Paciente p) {
+		
+		this.nome = p.getNome();
+		this.email = p.getEmail();
+		this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.dataNascimento = LocalDate.parse(p.getDataNascimento(), this.formatter);
+		this.id = p.getId();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDataNascimento() {
+		return this.dataNascimento.format(this.formatter);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+
+	public String toString() {
+		
+			String apresentacao = "Sr(a) " + this.nome + "\n(" + this.email + ")";
+		
+			return apresentacao;
+		} // end toString
+
+	private static String montaIdentificador() {
+		
+		numId++;
+		
+		return "PA" + numId;
+	} // end montaIdentificador
+	
+	} // end class
